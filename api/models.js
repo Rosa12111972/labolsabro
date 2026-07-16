0051f123231341702d0123fe64d38a706dcfd806
oscar <oscar.schmidt.rodriguez@gmail.com>
2026-07-16 11:03:40 +0200

    debug: endpoint para listar modelos Gemini disponibles

---
+++ api/models.js
@@ -0,0 +1,8 @@
+export default async function handler(req, res) {
+  const r = await fetch(
+    `https://generativelanguage.googleapis.com/v1beta/models?key=${process.env.GEMINI_API_KEY}`
+  );
+  const data = await r.json();
+  const nombres = (data.models || []).map(m => m.name);
+  return res.json({ modelos: nombres });
+}
